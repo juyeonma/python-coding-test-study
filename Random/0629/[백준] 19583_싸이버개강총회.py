13683ff1e94ada58787830b7b9ca4dad79a8b8ec
nuheajiohc <jaehun341012@gmail.com>
2023-06-29 21:53:49 +0900

    BOJ [#175][#176][#177][#178][#179][#180] 최재훈 - 생각보다 쉽지 않아서 고전했다.

---
+++ Random/0629/[백준] 19583_싸이버개강총회.py
@@ -0,0 +1,56 @@
+
+# 개강 총회전까지의 닉네임을 저장하고 개강총회 끝내고 스트리밍전까지의 닉네임을 찾아서 카운팅한다
+# 처음 풀이는 시간과 닉네임을 저장한 뒤 다시 for문 돌려서 찾았는데 그럴필요없이 바로하면된다.
+# 입력값을 input으로 받으면 시간초과가 난다
+
+import sys
+
+s,e,q = input().split()
+start_time = int(s[:2]+s[3:])
+end_time = int(e[:2]+e[3:])
+quit_time = int(q[:2]+q[3:])
+cnt = 0
+
+records = []  
+for line in sys.stdin:
+    time,name = line.split()
+    records.append((int(time[:2]+time[3:]),name))
+
+check=set()
+for record in records:
+    if record[0]<=start_time:
+        check.add(record[1])
+    if quit_time>=record[0]>=end_time:
+        if record[1] in check:
+            check.remove(record[1])
+            cnt+=1
+
+print(cnt)
+
+#메모리 54572kb 시간 188ms
+
+
+import sys
+
+s,e,q = input().split()
+start_time = int(s[:2]+s[3:])
+end_time = int(e[:2]+e[3:])
+quit_time = int(q[:2]+q[3:])
+cnt = 0
+ 
+check=set()
+for line in sys.stdin:
+    time,name = line.split()
+    time=int(time[:2]+time[3:])
+    if time<=start_time:
+        check.add(name)
+    if quit_time>=time>=end_time:
+        if name in check:
+            check.remove(name)
+            cnt+=1    
+
+print(cnt)
+
+#메모리 43548kb 시간 168ms
+
+#입력값 받는 부분 찾아보다가 시간을 안쟀다.
---
+++ Random/0629/[백준] 5212_지구 온난화.py
@@ -0,0 +1,83 @@
+# 각 땅마다 인접한 부분들을 bfs처럼 확인하고 각 구역을 기억해둔다
+# 기억해둔 곳을 돌면서 지도를 새롭게 갱신한다
+# 한 줄이 바다로 이루어져 있으면 다음 줄을 확인하는 방식으로
+# 왼쪽 오른쪽 위쪽 아래쪽 모두 확인해본다
+# 한줄이 바다로 이루어져있는지를 확인하는 것을 좀 더 잘 짜고 싶어서 고민하다가
+# 그냥 아래코드처럼 해버렸다.
+
+r,c = map(int,input().split())
+board = [list(input()) for _ in range(r)]
+
+dx = [0,0,1,-1]
+dy = [1,-1,0,0]
+
+byeland = []
+for i in range(r):
+    for j in range(c):
+        if board[i][j]=="X":
+            cnt=0
+            for dir in range(4):
+                nx=i+dx[dir]
+                ny=j+dy[dir]
+                if nx<0 or ny<0 or nx>=r or ny>=c:
+                    cnt+=1
+                    continue
+                if board[nx][ny]==".":
+                    cnt+=1
+                    continue
+                
+            if cnt>=3:
+                byeland.append((i,j))
+
+for x,y in byeland:
+    board[x][y]="."
+
+top=0
+bottom=r-1
+left=0
+right=c-1
+
+for i in range(r):
+    top=i
+    flag=True
+    for j in range(c):
+        if board[i][j]=="X":
+            flag=False
+            break
+    if not flag:
+        break
+
+for i in range(r-1,-1,-1):
+    bottom=i
+    flag=True
+    for j in range(c):
+        if board[i][j]=="X":
+            flag=False
+            break
+    if not flag:
+        break
+
+for i in range(c):
+    left=i
+    flag=True
+    for j in range(r):
+        if board[j][i]=="X":
+            flag=False
+            break
+    if not flag:
+        break
+
+for i in range(c-1,-1,-1):
+    right=i
+    flag=True
+    for j in range(r):
+        if board[j][i]=="X":
+            flag=False
+            break
+    if not flag:
+        break
+
+for i in range(top,bottom+1):
+    print("".join(board[i][left:right+1]))
+
+#38분
