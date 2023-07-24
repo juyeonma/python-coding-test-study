5678b7eb66e487f2649f4cd08f4960e97d0f47ea
nuheajiohc <jaehun341012@gmail.com>
2023-07-24 15:17:03 +0900

    Update [백준] 16987 - 계란으로 계란치기.py

--- Backtracking/0723/[백준] 16987 - 계란으로 계란치기.py
+++ Backtracking/0723/[백준] 16987 - 계란으로 계란치기.py
@@ -1,55 +1,55 @@
-첫번째 풀이로 풀고나서 다시 보니 뭔가 이상하다 왜 통과가 되는지 모르겠다.
-풀 때는 "됐군" 하고 풀었는데..
-너무 백트래킹의 기본틀에 갖혀서 풀려고 했던 것 같고 여러 조건을 다루는 건 처음이라 어려웠던 것 같다
-for문 안에서 조건을 세우려고 했었는데 이런 생각이 경직된 사고를 하게 만든 것 같다
+# 첫번째 풀이로 풀고나서 다시 보니 뭔가 이상하다 왜 통과가 되는지 모르겠다.
+# 풀 때는 "됐군" 하고 풀었는데..
+# 너무 백트래킹의 기본틀에 갖혀서 풀려고 했던 것 같고 여러 조건을 다루는 건 처음이라 어려웠던 것 같다
+# for문 안에서 조건을 세우려고 했었는데 이런 생각이 경직된 사고를 하게 만든 것 같다
 
-두번째 풀이는 주연님 풀이를 참고하였다. 
+# 두번째 풀이는 주연님 풀이를 참고하였다. 
 
-# n = int(input())
-# eggs = [list(map(int,input().split())) for _ in range(n)]
-# max_num = 0
-# vis = [0]*n
+# # n = int(input())
+# # eggs = [list(map(int,input().split())) for _ in range(n)]
+# # max_num = 0
+# # vis = [0]*n
 
-# def dfs(k):
-#     global max_num
-#     if k==n:
-#         max_num=max(max_num,vis.count(1))
-#         return
+# # def dfs(k):
+# #     global max_num
+# #     if k==n:
+# #         max_num=max(max_num,vis.count(1))
+# #         return
     
-#     if vis[k]:
-#         dfs(k+1)
-#         return
+# #     if vis[k]:
+# #         dfs(k+1)
+# #         return
 
-#     for i in range(n):
-#         if not vis[i] and i!=k:
-#             if eggs[k][0]<=eggs[i][1]:
-#                 vis[k]=1
-#                 if eggs[i][0]<=eggs[k][1]:
-#                     vis[i]=1
-#                     dfs(k+1)
-#                     vis[i]=0
-#                 else:
-#                     eggs[i][0]-=eggs[k][1]
-#                     dfs(k+1)
-#                     eggs[i][0]+=eggs[k][1]
-#                 vis[k]=0
-#             else:
-#                 eggs[k][0]-=eggs[i][1]
-#                 if eggs[i][0]<=eggs[k][1]:
-#                     vis[i]=1
-#                     dfs(k+1)
-#                     vis[i]=0
-#                 else:
-#                     eggs[i][0]-=eggs[k][1]
-#                     dfs(k+1)
-#                     eggs[i][0]+=eggs[k][1]
-#                 eggs[k][0]+=eggs[i][1]
+# #     for i in range(n):
+# #         if not vis[i] and i!=k:
+# #             if eggs[k][0]<=eggs[i][1]:
+# #                 vis[k]=1
+# #                 if eggs[i][0]<=eggs[k][1]:
+# #                     vis[i]=1
+# #                     dfs(k+1)
+# #                     vis[i]=0
+# #                 else:
+# #                     eggs[i][0]-=eggs[k][1]
+# #                     dfs(k+1)
+# #                     eggs[i][0]+=eggs[k][1]
+# #                 vis[k]=0
+# #             else:
+# #                 eggs[k][0]-=eggs[i][1]
+# #                 if eggs[i][0]<=eggs[k][1]:
+# #                     vis[i]=1
+# #                     dfs(k+1)
+# #                     vis[i]=0
+# #                 else:
+# #                     eggs[i][0]-=eggs[k][1]
+# #                     dfs(k+1)
+# #                     eggs[i][0]+=eggs[k][1]
+# #                 eggs[k][0]+=eggs[i][1]
 
-#     if k==n-1:
-#         max_num=max(max_num,vis.count(1))
-#         return
-# dfs(0)
-# print(max_num)
+# #     if k==n-1:
+# #         max_num=max(max_num,vis.count(1))
+# #         return
+# # dfs(0)
+# # print(max_num)
 
 n = int(input())
 eggs = [list(map(int,input().split())) for _ in range(n)]
@@ -72,12 +72,7 @@ def dfs(k,count):
         if eggs[i][0]>0 and i!=k:
             eggs[k][0]-=eggs[i][1]
             eggs[i][0]-=eggs[k][1]
-            temp = 0
-            if eggs[k][0]<=0:
-                temp+=1
-            if eggs[i][0]<=0:
-                temp+=1
-            dfs(k+1,count+temp)
+            dfs(k+1,count+(eggs[k][0]<=0)+(eggs[i][0]<=0))
             eggs[k][0]+=eggs[i][1]
             eggs[i][0]+=eggs[k][1]
 
