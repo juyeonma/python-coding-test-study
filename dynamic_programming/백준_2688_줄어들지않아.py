eb99cdf16365def97849d4221c36b0f460c823de
Yu-jiwon <jiwon7689@naver.com>
2023-08-07 14:37:51 +0900

    DP : [#260][#261] 유지원 - 어찌저찌 풀어서 백준에 제출하면 오류가 난다..

--- dynamic_programming/백준_2688_줄어들지않아.py
+++ dynamic_programming/백준_2688_줄어들지않아.py
@@ -1,33 +1,35 @@
 #런타임에러
-# t = int(input())
-# dp=[[0]*10 for _ in range(64)]
-
-# for j in range(10):
-#     dp[1][j]=1
-#     dp[2][j]=10-j
-
-# for i in range(2,64):
-#     dp[i][9]=1
-#     for j in range(9):
-#         dp[i][j]=sum(dp[i-1][j:])
-        
-# for _ in range(t):
-#     n = int(input())
-#     print(sum(dp[n]))
-
-# 어떻게 해야 런타임에러가 안날까..    
 t = int(input())
-  
+dp=[[1]*10 for _ in range(65)]
+
+for j in range(10):
+    dp[1][j]=1
+
+for i in range(2,65):
+    for j in range(9):
+        dp[i][j]=sum(dp[i-1][j:])
+print(dp[:4])       
 for _ in range(t):
     n = int(input())
-    
-    dp=[[1]*10 for _ in range(n+1)]
-
-    for j in range(10):
-        dp[2][j]=10-j
-
-    for i in range(2,n+1):
-        for j in range(9):
-            dp[i][j]=sum(dp[i-1][j:])
-            
     print(sum(dp[n]))
+
+# 어떻게 해야 런타임에러가 안날까..    
+# t = int(input())
+  
+# for _ in range(t):
+#     n = int(input())
+    
+#     dp=[[1]*10 for _ in range(n+1)]
+
+#     for j in range(10):
+#         dp[2][j]=10-j
+
+#     for i in range(2,n+1):
+#         for j in range(9):
+#             dp[i][j]=sum(dp[i-1][j:])
+            
+#     print(sum(dp[n]))
+
+# 코드길이 : 231 B
+# 시간 : 48 ms
+# 메모리 : 31256 KB
